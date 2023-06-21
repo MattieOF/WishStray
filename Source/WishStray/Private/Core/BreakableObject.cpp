a1e4b10cfd79ecc536529438dda743dad0efa778
Matt Ware <mattbware3@gmail.com>
2023-06-21 03:17:44 +0100

    Upgrade to UE5.2

---
+++ Source/WishStray/Private/Core/BreakableObject.cpp
@@ -0,0 +1,59 @@
+﻿// copyright lololol
+
+#include "Core/BreakableObject.h"
+
+#include "GeometryCollection/GeometryCollectionComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+ABreakableObject::ABreakableObject()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	RootComponent = MeshComp;
+}
+
+void ABreakableObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (Mesh)
+		MeshComp->SetStaticMesh(Mesh);
+
+	MeshComp->OnComponentHit.AddDynamic(this, &ABreakableObject::OnMeshHit);
+}
+
+void ABreakableObject::Break()
+{
+	if (bHasBroken)
+		return;
+
+	if (BreakSound)
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), BreakSound, MeshComp->GetComponentLocation(), MeshComp->GetComponentRotation());
+
+	GeoCollectionComp = Cast<UGeometryCollectionComponent>(
+		AddComponentByClass(UGeometryCollectionComponent::StaticClass(), false, FTransform::Identity, true));
+	GeoCollectionComp->SetRestCollection(GeoCollection);
+	FinishAddComponent(GeoCollectionComp, false, FTransform::Identity);
+	GeoCollectionComp->CrumbleActiveClusters();
+
+	MeshComp->DestroyComponent();
+	RootComponent = GeoCollectionComp;
+	
+	bHasBroken = true;
+}
+
+void ABreakableObject::OnMeshHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (HitComponent->GetComponentVelocity().Length() >= BreakSpeed || OtherComp->GetComponentVelocity().Length() >= BreakSpeed)
+		Break();
+}
+
+#if WITH_EDITOR
+void ABreakableObject::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	if (PropertyChangedEvent.Property->GetNameCPP() == "Mesh")
+		MeshComp->SetStaticMesh(Mesh);
+	
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+#endif
---
+++ Source/WishStray/Public/Core/BreakableObject.h
@@ -0,0 +1,54 @@
+﻿// copyright lololol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "BreakableObject.generated.h"
+
+class UGeometryCollection;
+class UGeometryCollectionComponent;
+
+/**
+ * An object that can be broken.
+ */
+UCLASS()
+class WISHSTRAY_API ABreakableObject : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABreakableObject();
+
+	virtual void BeginPlay() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void Break();
+
+	void OnMeshHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float BreakSpeed = 1000;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UGeometryCollection* GeoCollection;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* MeshComp;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UGeometryCollectionComponent* GeoCollectionComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* BreakSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bHasBroken = false;
+};
